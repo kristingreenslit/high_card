b05910e80e75e3cb947cb1c78fa99089d222f381
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-18 20:34:28 -0400

    create array of shuffled cards

--- high_card.rb
+++ high_card.rb
@@ -2,21 +2,25 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-# cardFront = ranks.shuffle
-# cardBack = suits.shuffle
-# puts cardFront + cardBack
-
-new_deck = ranks.product(suits)
-new_deck.shuffle
-# puts new_deck
-
 players = []
+fresh_deck = []
 
-puts "1 player so far. Enter player names, then type 'play' to start the game"
+deck = ranks.product(suits)
+# pairs elements in 'ranks' and 'suits' arrays
+deck_shuffled = deck.shuffle
+# shuffles pairs
+fresh_deck.push(deck_shuffled)
+# pushes shuffled cards into a new array
+
+# need function to deal an element from fresh_deck to each player
+
+
+puts "Enter a player's name, or type 'play' to immediately start the game"
 userInput = gets.chomp
   if userInput != 'play'
     players.push(userInput)
-    puts players
+#loop through asking for player names until user enters "play"
   else userInput == 'play'
+    players.each { |player| puts 'Player(s) in this game: ' + player }
     puts "let's play!"
   end
