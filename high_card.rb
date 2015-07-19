fcaca0571246620d921ea0bcf8954b2fc99ace64
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-19 19:56:06 -0400

    all is not fair in love and war-string/integer issue?

--- high_card.rb
+++ high_card.rb
@@ -16,23 +16,27 @@ players.push(userInput)
         userInput = gets.chomp
         players.push(userInput)
   else userInput == 'play'
-    userInput.clear
-    # ^ remove 'play', the userInput that was pushed into the array
-    players.insert(0 , "Player 1")
-    players.insert(1 , "Player 2")
-    # ^ push player names into specific index positions since .clear retains [0] with empty value
+     userInput.clear
+     # ^ remove 'play', the userInput that was pushed into the array
+     players.insert(0 , "Player 1")
+     players.insert(1 , "Player 2")
+     # ^ push player names into specific index positions since .clear retains [0] with empty value
   end
 
-card_1 = "#{deck_shuffled[0]}"
-card_2 = "#{deck_shuffled[1]}"
+card_1 = deck_shuffled[0].inspect
+card_2 = deck_shuffled[1].inspect
+
+# 6 of spades[0] beat 10 of hearts[1]
+# 9 of spades[1] beat King of diamonds[0]
+# is one issue that integer is beating string in each card pair? [8, "hearts"] [Queen, hearts]
 
 puts players[0] + ": " + card_1
 puts players[1] + ": " + card_2
 
-if card_1 < card_2
+if card_1 > card_2
   puts players[0] + " " + "wins!"
-elsif card_1 > card_2
+elsif card_1 < card_2
   puts players[1] + " " + "wins!"
 else card_1 == card_2
-  puts "It's a tie between" + players[0] + " and" + players[1] + " !"
+  puts "It's a tie between " + players[0] + " and " + players[1] + "!"
 end
