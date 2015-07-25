88cfdb424864e6214e8504b222f6fee9c2c22390
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-25 15:27:37 -0400

    winning card evaluates correctly

--- high_card.rb
+++ high_card.rb
@@ -1,8 +1,8 @@
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+suits = [ "Hearts", "Spades", "Clubs", "Diamonds" ]
 
 players = []
-
+# ^ array that player names are pushed into when user enters them
 deck = ranks.product(suits)
 # ^ pair together elements in 'ranks' and 'suits' arrays
 deck_shuffled = deck.shuffle
@@ -15,28 +15,32 @@ players.push(userInput)
       puts "Enter second player's name"
         userInput = gets.chomp
         players.push(userInput)
-  else userInput == 'play'
+  else
      userInput.clear
      # ^ remove 'play', the userInput that was pushed into the array
      players.insert(0 , "Player 1")
      players.insert(1 , "Player 2")
-     # ^ push player names into specific index positions since .clear retains [0] with empty value
+     # ^ push player names into specific index positions in players array since .clear retains [0] with empty value
   end
 
-card_1 = deck_shuffled[0].inspect
-card_2 = deck_shuffled[1].inspect
+card_1 = deck_shuffled[0]
+card_2 = deck_shuffled[1]
 
-# 6 of spades[0] beat 10 of hearts[1]
-# 9 of spades[1] beat King of diamonds[0]
-# is one issue that integer is beating string in each card pair? [8, "hearts"] [Queen, hearts]
+rank_1 = card_1[0]
+rank_2 = card_2[0]
+# ^ var created so that index position [0] can be evaluated for each card that's dealt to a player.
+# the suit for each card (index position [1]) will never need to be evaluated when determining a winner
 
-puts players[0] + ": " + card_1
-puts players[1] + ": " + card_2
+puts "#{players[0]}: #{rank_1} of #{card_1[1]}"
+puts "#{players[1]}: #{rank_2} of #{card_2[1]}"
+# ^ editing puts statements allowed for the removal of .inspect from var's card_1 and card_2
 
-if card_1 > card_2
-  puts players[0] + " " + "wins!"
-elsif card_1 < card_2
-  puts players[1] + " " + "wins!"
-else card_1 == card_2
-  puts "It's a tie between " + players[0] + " and " + players[1] + "!"
+if ranks.index(rank_1) > ranks.index(rank_2)
+  puts "#{players[0]} wins!"
+elsif ranks.index(rank_1) < ranks.index(rank_2)
+  puts "#{players[1]} wins!"
+else
+  puts "It's a tie between #{players[0]} and #{players[1]}!"
+  # puts "It's a tie between "players[0] + " and " + players[1] + "!"
+  # ^ changed all puts statements to mirror standard ruby notation (used to look more like js)
 end
